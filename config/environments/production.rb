5b96799948d5d91605564862e2eba922e9434f48
Carlos Torrealba <calrrox@gmail.com>
2017-05-30 07:13:15 -0400

    init class

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "forms_bloque_4_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "bootstrap_bloque_5_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
